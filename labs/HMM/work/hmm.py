9952007804349713b222fea151811c6b060c901b
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-11-26 05:24:41 -0800

    forward improve

--- labs/HMM/work/hmm.py
+++ labs/HMM/work/hmm.py
@@ -38,7 +38,7 @@ class HMM:
 
         for t in range(L):
             if t == 0:
-                alpha[:, t] = (self.pi * self.B[:, O[t]].T).T
+                alpha[:, t] = (self.B[:, O[t]].T * self.pi).T
             else:
                 alpha[:, t] = self.B[:, O[t]] * np.dot(alpha[:, t - 1].T, self.A).T
 
